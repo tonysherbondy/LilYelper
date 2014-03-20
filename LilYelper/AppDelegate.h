23a052a4749843fec68038758b9a0b17a20bd098
Anthony Sherbondy <ts1235813@gmail.com>
2014-03-20 16:25:09 -0700

    created project LilYelper

---
+++ LilYelper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LilYelper
+//
+//  Created by Anthony Sherbondy on 3/20/14.
+//  Copyright (c) 2014 Anthony Sherbondy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
